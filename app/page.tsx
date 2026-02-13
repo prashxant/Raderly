5943132e23f1c1547c5050619bb394b07fb222bc
prashxant <notxdead0@gmail.com>
2026-02-13 09:51:34 +0530

    hi

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UploadCard } from "@/components/UploadCard";
-
-export default function Home() {
-  return (
-    <div className="h-screen w-full flex flex-col justify-center items-center">
-      <UploadCard />
-    </div>
-  );
-}
